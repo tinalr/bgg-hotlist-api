13b21afeb7950a9d3e4147061f8b0844f8f11c64
Christina Rontell <clrontell@gmail.com>
2022-08-08 21:58:40 -0500

    BGG api, get hotness list

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+// Need to find a way to parse XML data to JSON
+// Have tried npm packages: xml2js, xml-to-jason, react-xml-parser
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(
+      "https://boardgamegeek.com/xmlapi2/hot?type=boardgame",
+      { "Content-Type": "application/xml; charset=utf-8" }
+    ).then((response) => {
+      const resData = response.data;
+      setPosts(resData.toArray());
+    })
+  }, [])
+
+  console.log(posts);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello World</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>Board Game Geek's "The Hotness":</h1>
+      {posts.map((post) => {
+
+        return (
+          <div>
+            <h2>{post}</h2>
+          </div>
+        );
+      })}
+
     </div>
   )
 }
 
-export default App
+export default App;
