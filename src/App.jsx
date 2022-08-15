57210c912d3a6fb7374add93d9e9df0e64d3418f
Christina Rontell <clrontell@gmail.com>
2022-08-15 11:57:07 -0500

    set up x2js

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
 import './App.css'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import X2JS from 'x2js';
 
-// Need to find a way to parse XML data to JSON
+
+// https://boardgamegeek.com/wiki/page/BGG_XML_API2#
+// Need to find a way to parse XML data to JSON ???
 // Have tried npm packages: xml2js, xml-to-jason, react-xml-parser
 
-function App() {
+const App = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    //https://boardgamegeek.com/wiki/page/BGG_XML_API2#
-    axios.get(
-      "https://boardgamegeek.com/xmlapi2/hot?type=boardgame", 
-      { "Content-Type": "application/xml; charset=utf-8" }
-    ).then((response) => {
-      const resData = response.data;
-      setPosts(resData.toArray());
-    })
-  }, [])
+    getResponse();
+  }, []);
 
-  console.log(posts);
+  const getResponse = async () => {
+    const response = await axios.get(
+      "https://boardgamegeek.com/xmlapi2/hot?type=boardgame",
+    );
+    if (response.data) {
+      let x2js = new X2JS();
+      let document = x2js.xml2js(response.data);
+      //console.log(document.items.item)
+      setPosts(document.items.item)
+    }
+  }; console.log(posts);
 
   return (
     <div className="App">
-
       <h1>Board Game Geek's "The Hotness":</h1>
       {posts.map((post) => {
-
         return (
-          <div>
-            <h2>{post}</h2>
+          <div key={post._id}>
+            <h2>#{post._rank} {post.name._value}</h2>
           </div>
         );
       })}
-
     </div>
   )
 }
