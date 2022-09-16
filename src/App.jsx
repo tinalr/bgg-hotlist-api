2e4049e5a76ae7b28501e449b0ff822fe6379798
Christina Rontell <clrontell@gmail.com>
2022-09-15 23:35:05 -0500

    card formatting joy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 //import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+//import CardActions from '@mui/material/CardActions';
+//import CardContent from '@mui/material/CardContent';
+//import CardMedia from '@mui/material/CardMedia';
+//import Typography from '@mui/material/Typography';
 import './App.css'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import X2JS from 'x2js';
-import { Container, maxWidth } from '@mui/system';
-import { Grid } from '@mui/material';
+//import { Container, maxWidth } from '@mui/system';
+//import { Grid } from '@mui/material';
 import { CssVarsProvider } from '@mui/joy/styles';
 import AspectRatio from '@mui/joy/AspectRatio';
-import Card from '@mui/joy/Card';
+//import Card from '@mui/joy/Card';
+import { Card, CardOverflow, CardContent, Typography } from '@mui/joy';
 
 
 const App = () => {
@@ -43,17 +44,25 @@ const App = () => {
 
         {posts.map((post) => {
           return (
-            <Card key={post._id}>
+            <Card
+              key={post._id}
+              sx={{
+                '--Card-padding': '16px'
+              }}
+            >
 
 
+              <CardOverflow>
+                <AspectRatio ratio='1/1' sx={{ maxWidth: 100 }}>
+                  <img src={post.thumbnail._value} />
+                </AspectRatio>
+              </CardOverflow>
 
-              <AspectRatio ratio='1/1' sx={{ maxWidth: 200 }}>
-                <img src={post.thumbnail._value} />
-              </AspectRatio>
-
-              <h3>#{post._rank} {post.name._value}</h3>
-
-
+              <CardContent>
+                <Typography>
+                  #{post._rank} {post.name._value}
+                </Typography>
+              </CardContent>
 
             </Card>
           );
